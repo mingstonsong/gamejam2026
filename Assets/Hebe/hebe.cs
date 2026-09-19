ddcc72d5bfbdfe04328e1f4adfcec5eddece01c8
Mingston Song <152594064+Exonuts@users.noreply.github.com>
2026-09-19 12:54:51 +1200

    Resolved conflicts

--- Assets/Hebe/hebe.cs
+++ Assets/Hebe/hebe.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class hebe : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
